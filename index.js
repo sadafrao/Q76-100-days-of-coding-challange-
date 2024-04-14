1b6307e16f1a75628026121e0b28125ce35bccee
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:50:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q76
+// this function adds two numbers and retun the result
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(10, 20));
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q76
+// this function adds two numbers and retun the result
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+console.log(add(10, 20));
